3ed225551b9968b49cdc51213040173b47fbab5c
Enner Pérez <ennerperez@gmail.com>
2019-11-14 17:07:14 -0500

    Example Integrration & Update

--- Wkhtmltopdf.NetCore.Example/Startup.cs
+++ Wkhtmltopdf.NetCore.Example/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Wkhtmltopdf.NetCore;
 
 namespace Wkhtmltopdf
@@ -19,19 +20,25 @@ namespace Wkhtmltopdf
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRazorPages().SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.AddWkhtmltopdf();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "default", pattern: "api/{controller=TestViews}/{action=Get}/{id?}");
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
