4a564118ff211b5675888f5ed071ef903e5c72a7
gurustron <gurustronpublic@gmail.com>
2020-09-08 20:34:59 +0300

    Added a smoke test.

---
+++ Wkhtmltopdf.NetCore.Test/SmokeTest.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+
+namespace Wkhtmltopdf.NetCore.Test
+{
+    public class SmokeTest
+    {
+        [Test]
+        public void Test1()
+        {
+            WkhtmltopdfConfiguration.RotativaPath = AppDomain.CurrentDomain.BaseDirectory + "Rotativa";
+            var generatePdf = new GeneratePdf(null);
+            generatePdf.GetPDF("<p><h1>Hello World</h1>This is html rendered text</p>");
+        }
+    }
+}
