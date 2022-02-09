26d2a7478c467bae34a6cb33d580d38fbb8e83fb
Lucca Faria Ferri <habboclub97@gmail.com>
2022-02-09 18:26:11 -0300

    update

--- PdfApi/Program.cs
+++ PdfApi/Program.cs
@@ -4,7 +4,10 @@ using Wkhtmltopdf.NetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.WebHost.UseKestrel(kestrel =>
+{
+    kestrel.ListenAnyIP(80);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
